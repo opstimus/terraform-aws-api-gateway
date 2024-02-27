16ce9f4f5c38110a426994060b9f3e827687fa0a
Checkboxer <checkboxer@friedlemons.org>
2024-02-28 00:56:35 +0530

    cors

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 resource "aws_apigatewayv2_api" "main" {
   name          = "${var.project}-${var.environment}-${var.name}"
   protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_origins = ["*"]
+  }
 }
